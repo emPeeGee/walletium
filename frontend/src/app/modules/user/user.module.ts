a2b134f4894e9ce89b0f9163005605f89ecea5b8
Mihail Popovici <eempeegee@gmail.com>
2021-07-31 15:26:38 +0300

    feat: Remode account-add-card, use rem units

--- frontend/src/app/modules/user/user.module.ts
+++ frontend/src/app/modules/user/user.module.ts
@@ -9,7 +9,6 @@ import { EffectsModule } from '@ngrx/effects';
 import { AccountCardComponent } from './components/account-card/account-card.component';
 import { AccountSaveModalComponent } from './components/account-save-modal/account-save-modal.component';
 import { AccountsLayoutComponent } from './components/accounts-layout/accounts-layout.component';
-import { AccountAddCardComponent } from './components/account-add-card/account-add-card.component';
 import { AccountDetailComponent } from './components/account-detail/account-detail.component';
 import { LabelsLayoutComponent } from './components/labels-layout/labels-layout.component';
 import { LabelCardComponent } from './components/label-card/label-card.component';
@@ -23,7 +22,6 @@ import { RecordSaveModalComponent } from './components/record-save-modal/record-
     AccountCardComponent,
     AccountSaveModalComponent,
     AccountsLayoutComponent,
-    AccountAddCardComponent,
     AccountDetailComponent,
     LabelsLayoutComponent,
     LabelCardComponent,
