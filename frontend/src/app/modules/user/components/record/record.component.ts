c9f10f8ec076b3ddaea26254c9245b3fe2de3669
Mihail Popovici <eempeegee@gmail.com>
2021-08-03 22:18:27 +0300

    feat(): Init to work on record component

--- frontend/src/app/modules/user/components/record/record.component.ts
+++ frontend/src/app/modules/user/components/record/record.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { RecordsService } from 'src/app/core/services/api/records.service';
 import { Record } from '../../models/record.model';
 
 @Component({
@@ -7,14 +10,41 @@ import { Record } from '../../models/record.model';
   templateUrl: './record.component.html',
   styleUrls: ['./record.component.scss']
 })
-export class RecordComponent implements OnInit {
+export class RecordComponent implements OnInit, OnDestroy {
   public record: Record | null = null;
+  public recordForm: FormGroup | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  private recordSubscription: Subscription | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private recordsService: RecordsService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      console.log(params.get('id'));
+      const id: string = params.get('id') ?? '';
+
+      this.recordSubscription = this.recordsService.getRecordById(id).subscribe(record => {
+        this.record = record;
+
+        this.recordForm = this.formBuilder.group({
+          id: [record.id],
+          type: [record.type],
+          amount: [record.amount],
+          updatedDate: [record.updatedDate],
+          payee: [record.payee],
+          note: [record.note],
+          place: [record.place],
+          category: [''],
+          labels: ['']
+        });
+      });
     });
+  }
+
+  ngOnDestroy(): void {
+    this.recordSubscription?.unsubscribe();
   }
 }
